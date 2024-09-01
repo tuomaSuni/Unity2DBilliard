e8b121bf40068f99af96c92d093c9f59985af4d5
tuomaSuni <tuomas.suni@mail.com>
2024-09-01 20:30:13 +0300

    Code updates

---
+++ Assets/Scripts/lineLogic.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class DottedLineController : MonoBehaviour
+{
+    [SerializeField] private GameObject dotPrefab;
+    [SerializeField] private Transform startPoint;
+    [SerializeField] private float dotSpacing;
+
+    private void Update()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.z = 0f;
+
+        foreach (Transform child in transform)
+        {
+            Destroy(child.gameObject);
+        }
+
+        float distance = Vector3.Distance(startPoint.position, mousePosition);
+        int dotCount = Mathf.CeilToInt(distance / dotSpacing);
+
+        Vector3 direction = (mousePosition - startPoint.position).normalized;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        for (int i = 0; i <= dotCount; i++)
+        {
+            Vector3 dotPosition = Vector3.Lerp(startPoint.position, mousePosition, i / (float)dotCount);
+            GameObject dot = Instantiate(dotPrefab, dotPosition, Quaternion.Euler(0, 0, angle), transform);
+        }
+
+        startPoint.rotation = Quaternion.Euler(0, 0, angle);
+    }
+}
--- Assets/Scripts/rockLogic.cs
+++ Assets/Scripts/rockLogic.cs
@@ -5,28 +5,19 @@ using UnityEngine;
 public class rockLogi : MonoBehaviour
 {
     private Rigidbody2D rb;
+    [SerializeField] private float pushForce = 10f;
 
-    // Force multiplier
-    public float pushForce = 10f;
-
-    // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            // Get mouse position in world coordinates
             Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-            // Calculate the direction from the rock to the mouse position
             Vector2 direction = (mousePosition - rb.position).normalized;
-
-            // Apply force in that direction
             rb.velocity = direction * pushForce;
         }
     }
